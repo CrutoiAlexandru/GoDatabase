cac059000355ac073700e211e579de6813bb5cc7
burnscopesali <crutoi2@yahoo.com>
2020-08-11 19:29:08 +0300

    Change readme

--- Database/TestDB.sql
+++ Database/TestDB.sql
@@ -1,5 +1,4 @@
-update user_data
-set user_name = 'name'
-where user_id = 1;
+update user_data set user_id = 1 where user_name = 'Mirel Costin';
 select * from user_data;
-delete from user_data where user_name = 'CLOSE'
+delete from user_data where user_name = 'CLOSE';
+SELECT user_id, LAST_VALUE(user_id) OVER (ORDER BY user_id) last_id FROM user_data;
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # GoDatabase
 
 A terminal controlled database made with golang
+
+It is very basic, and also it is more of an experimentation on how to use mysql with go.
+Also the id making has to get fixed and there should be better code organisation as well as better names for the variables.
